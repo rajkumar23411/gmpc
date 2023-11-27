d7f9446ea614174861d4b9ae997d8efadbcc0b5e
Rajkumar Kalita <rajkumarriyan001@gmail.com>
2023-11-27 22:06:50 +0530

    updating ui

--- src/_root/pages/PatientsCare.tsx
+++ src/_root/pages/PatientsCare.tsx
@@ -1,3 +1,4 @@
+import FreeService from "@/components/shared/FreeService";
 import PageHeading from "@/components/shared/PageHeading";
 import TopicHeading from "@/components/shared/TopicHeading";
 
@@ -5,17 +6,17 @@ const PatientsCare = () => {
     return (
         <div>
             <PageHeading heading="Patient's Care" />
-            <div className="p-20 flex gap-10">
-                <div className="w-[38rem]">
+            <div className="p-8 md:p-12 lg:p-20 flex flex-col md:flex-row gap-10">
+                <div className="w-full h-96 lg:h-[30rem] lg:w-[38rem]">
                     <img
                         src="/assets/bg-patience-care.jpg"
                         alt="patient's care"
-                        className="h-[36rem] w-max rounded-xl object-cover drop-shadow-xl"
+                        className="h-full w-max rounded-xl object-cover drop-shadow-xl"
                     />
                 </div>
                 <div className="w-full">
                     <TopicHeading heading="We Do Care of Your Health" />
-                    <p className="font-sans text-purple-950 text-lg mt-6">
+                    <p className="font-sans text-purple-950 text-base md:text-lg mt-4 md:mt-6">
                         GMPC provides care that is free from harm, minimizes
                         redundancy and waste, allows timely access to needed
                         services, follows best practices, and incorporates
@@ -23,6 +24,7 @@ const PatientsCare = () => {
                     </p>
                 </div>
             </div>
+            <FreeService />
         </div>
     );
 };
--- src/components/shared/TopicHeading.tsx
+++ src/components/shared/TopicHeading.tsx
@@ -1,6 +1,6 @@
 const TopicHeading = ({ heading }: { heading: string }) => {
     return (
-        <div className="h-max py-4 bg-gradient-to-r from-main to-[#386199] border-l-[10px] border-red-600 flex items-center pl-10 text-2xl md:text-4xl text-white">
+        <div className="h-max py-4 bg-gradient-to-r from-main to-[#386199] border-l-[10px] border-red-600 flex items-center pl-10 text-2xl md:text-3xl lg:text-4xl text-white">
             {heading}
         </div>
     );
